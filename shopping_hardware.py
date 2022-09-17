b917da9fa40be1d59ce46173c3433f281b0be9fc
svetl <svetlio.zhelev36@outlook.com>
2022-09-17 19:20:14 +0300

    NewCommit

---
+++ shopping_hardware.py
@@ -0,0 +1,10 @@
+budget = float(input())
+
+video_cards_count = int(input())
+processors_count = int(input())
+ram_memory_count = int(input())
+
+video_card_price = video_cards_count * 250
+processors_price = video_card_price * .35
+ram_memory_price = video_card_price * .1
+
