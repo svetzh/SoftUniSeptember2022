ac319616e70ba72b444c6c21954b4e845828fc82
svetl <svetlio.zhelev36@outlook.com>
2022-09-20 10:54:18 +0300

    Advanced_conditions

--- 03. Advanced_Conditions/day_of_week.py
+++ 03. Advanced_Conditions/day_of_week.py
@@ -1,3 +1,19 @@
-number = int(input("Enter number from 1 - 7: "))
+number = int(input())
 
-
+if 1 <= number <= 7:
+    if number == 1:
+        print("Monday")
+    elif number == 2:
+        print("Tuesday")
+    elif number == 3:
+        print("Wednesday")
+    elif number == 4:
+        print("Thursday")
+    elif number == 5:
+        print("Friday")
+    elif number == 6:
+        print("Saturday")
+    else:
+        print("Sunday")
+else:
+    print("Error")
---
+++ 03. Advanced_Conditions/type_animal.py
@@ -0,0 +1,8 @@
+animal_name = input()
+
+if animal_name == "snake" or animal_name == "crocodile" or animal_name == "tortoise":
+    print("reptile")
+elif animal_name == "dog":
+        print("mammal")
+else:
+    print("unknown")
