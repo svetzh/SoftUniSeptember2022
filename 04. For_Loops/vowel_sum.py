579c064f3bca670b4c8f628090dcc343ebf5403c
svetl <svetlio.zhelev36@outlook.com>
2022-10-01 15:10:14 +0300

    Advanced_conditions

--- 04. For_Loops/vowel_sum.py
+++ 04. For_Loops/vowel_sum.py
@@ -1,15 +1,32 @@
+# text = input()
+# suma = 0
+#
+# for char in range(0, len(text)):
+#     if text[char] == "a":
+#         suma += 1
+#     if text[char] == "e":
+#         suma += 2
+#     if text[char] == "i":
+#         suma += 3
+#     if text[char] == "o":
+#         suma += 4
+#     if text[char] == "u":
+#         suma += 5
+# print(suma)
+
 text = input()
 suma = 0
 
-for char in range(0, len(text)):
-    if text[char] == "a":
+for symbol in range(0, len(text)):
+    chars_of_word = (text[symbol])
+    if chars_of_word == "a":
         suma += 1
-    if text[char] == "e":
+    if chars_of_word == "e":
         suma += 2
-    if text[char] == "i":
+    if chars_of_word == "i":
         suma += 3
-    if text[char] == "o":
+    if chars_of_word == "o":
         suma += 4
-    if text[char] == "u":
+    if chars_of_word == "u":
         suma += 5
 print(suma)
