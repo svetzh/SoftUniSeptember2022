10748b5b8c9c36818bd6a6a42114862ec15805d3
svetl <svetlio.zhelev36@outlook.com>
2022-10-04 18:26:03 +0300

    Advanced_conditions

--- 05. While_Loops/_03_sum_numbers.py
+++ 05. While_Loops/_03_sum_numbers.py
@@ -1,1 +1,8 @@
-dad
+initial_number = int(input())
+sum_of_all_digits = 0
+digit = 0
+while initial_number > sum_of_all_digits:
+    digit = int(input())
+    sum_of_all_digits += digit
+
+print(sum_of_all_digits)
